ade46cf759fff9d757f86961558858fceb14792d
NidhitheCoder <nidhpv@gmail.com>
2021-06-06 22:21:15 +0530

    View page created and change api routes in different file

--- expressJS/index.js
+++ expressJS/index.js
@@ -1,7 +1,6 @@
 //  To run : npm run dev
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
 const logger = require('./middleware/logger');
 
 const app = express();
@@ -14,23 +13,11 @@ const app = express();
 // // Init middleware
 // app.use(logger);
 
-// Gets all members
-app.get('/api/members', (req, res)=> res.json(members) );
-
-// Get single member
-app.get('/api/members/:id', (req, res) => {
-  // res.send(req.params.id);
-  const found = members.some(member => member.id === parseInt(req.params.id));
-
-  if(found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({msg: `No member with id ${req.params.id}`}); 
-  }
-});
-
 // Set a static folder
 app.use(express.static(path.join(__dirname,'public')));
+
+// Members api routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
