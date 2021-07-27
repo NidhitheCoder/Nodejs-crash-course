daa0676881ea65b9e85c97e243890f071ab5d2de
NidhitheCoder <nidhpv@gmail.com>
2021-07-27 23:38:52 +0530

    Add api form and api members list to handlebar home route

--- expressJS/index.js
+++ expressJS/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
@@ -23,7 +24,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // If we need to pass form then you can add it inside the object.
 
 // Homepage route
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index', {
+  title: "Member App",
+  members,
+}));
 
 // Members api routes
 app.use('/api/members', require('./routes/api/members'));
--- expressJS/routes/api/members.js
+++ expressJS/routes/api/members.js
@@ -32,6 +32,7 @@ router.post('/', (req, res) => {
 
   members.push(newMember);
   res.json(members);
+  // res.redirect('/');
 });
 
 // Update Member
