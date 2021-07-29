45b300ce2bdb929800f2c5a119233fe1055e97f5
NidhitheCoder <nidhpv@gmail.com>
2021-07-29 23:59:02 +0530

    Basic working of app and connected to the mongoose

---
+++ rest-api-with-node/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true  });
+const db = mongoose.connection; // mongodb connection started
+db.on('error', (error) => console.error(erro));
+db.once('open', () => console.log('Connected to the database'));
+
+app.use(express.json());
+const subscribersRouter = require('./routes/subscribers.js')
+
+app.listen(8000, () => console.log('Server started'));
