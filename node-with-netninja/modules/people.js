f89d4c017cef5ef3d79d3c5001eff8c242ca705b
NidhitheCoder <nidhpv@gmail.com>
2021-08-11 00:09:21 +0530

    modules exports  and core modules exports

---
+++ node-with-netninja/modules/people.js
@@ -0,0 +1,11 @@
+const people = ['Janni', 'Mottu', 'Juliya', 'Helen'];
+const ages = [46, 56, 12, 83];
+
+console.log(people);
+
+// module.exports = 'Hello';
+// module.exports = people;
+module.exports = {
+  people,
+  ages
+};
