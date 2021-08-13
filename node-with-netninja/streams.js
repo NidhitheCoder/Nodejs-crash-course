a6acd042f0c7766def45768babbc0cc4ff3916ef
NidhitheCoder <nidhpv@gmail.com>
2021-08-13 08:32:33 +0530

    write buffer stream

--- node-with-netninja/streams.js
+++ node-with-netninja/streams.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./node-with-netninja/docs/blog3.txt', {encoding: 'utf8' });
+const readStream = fs.createReadStream('./docs/blog3.txt', {encoding: 'utf8' });
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
+
+// without pipe
 readStream.on('data', (chunk) => {
   console.log('+++++++++New Chunk+++++++++++');
   // console.log(chunk.toString());
   console.log(chunk);
+  writeStream.write('\n NEW CHUNK \n');
+  writeStream.write(chunk);
 });
+
+// with pipe we can write a stream with less code 
+readStream.pipe(writeStream);
