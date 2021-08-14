1a3a94eed6c352341978f2d76acfb82f8fa178ab
NidhitheCoder <nidhpv@gmail.com>
2021-08-14 23:30:43 +0530

    send html file as a response

--- node-with-netninja/client-and-server/server.js
+++ node-with-netninja/client-and-server/server.js
@@ -1,16 +1,30 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
   // set content header type
   // res.setHeader('content-type', 'text/plain');
+
   // res.write('Hellow Ninjaz..');
   res.setHeader('content-type', 'text/html');
 
-  res.write('<head><link rel="stylesheet" href="#"></head>');
-  res.write('<p>Hellow ninjazz<p>');
-  res.write('<p>Hello again ninjaz</p>');
-  res.end();
+  // res.write('<head><link rel="stylesheet" href="#"></head>');
+  // res.write('<p>Hellow ninjazz<p>');
+  // res.write('<p>Hello again ninjaz</p>');
+  // res.end();
+
+  // send html file as response.
+  fs.readFile('./views/index.html', (err, data) => {
+    if(err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.end(data);
+    }
+  });
+
 });
 
 server.listen(5000, 'localhost', () => {
