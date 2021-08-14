4a12be6dd004992b7b712e659c1e958c4c9a75eb
NidhitheCoder <nidhpv@gmail.com>
2021-08-14 23:19:01 +0530

    request and response objects

--- node-with-netninja/client-and-server/server.js
+++ node-with-netninja/client-and-server/server.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('Request made');
+  console.log(req.url, req.method);
+  // set content header type
+  // res.setHeader('content-type', 'text/plain');
+  // res.write('Hellow Ninjaz..');
+  res.setHeader('content-type', 'text/html');
+
+  res.write('<head><link rel="stylesheet" href="#"></head>');
+  res.write('<p>Hellow ninjazz<p>');
+  res.write('<p>Hello again ninjaz</p>');
+  res.end();
 });
 
 server.listen(5000, 'localhost', () => {
-  console.log('Listening for request on port 3000');
+  console.log('Listening for request on port 5000');
 });
