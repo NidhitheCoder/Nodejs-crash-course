71bb576e0b4136aa4ebe9b39262972ee5d36d6b3
NidhitheCoder <nidhpv@gmail.com>
2021-08-25 23:58:51 +0530

    changes added

--- node-with-netninja/mongo-db/app.js
+++ node-with-netninja/mongo-db/app.js
@@ -8,8 +8,8 @@ const app = express();
 // mongodb connections
 const dbURI = "mongodb+srv://admin:<password>@cluster0.xglsn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true});
-then((result) => console.log('Connected to database'))
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => console.log('Connected to database'))
 .catch((err) => console.log(err));
 
 // register view engine 
