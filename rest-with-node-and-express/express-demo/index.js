4ff277fff33f02b1b00b06bcfe2bcb485e106ed7
Nidheesh PV <nidheesh@vonnue.com>
2023-12-29 23:15:03 +0530

    feat: update rest apis with parameters and query parameters

--- rest-with-node-and-express/express-demo/index.js
+++ rest-with-node-and-express/express-demo/index.js
@@ -10,4 +10,22 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3, 4])
 })
 
-app.listen(3000, () => console.log('Listening the port 3000'))
+// /api/courses/1 - single parameters
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+// Request with multiple parameters
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params)
+})
+
+// Request with query parameters
+app.get('/api/comments', (req, res) => {
+    res.send(req.query)
+})
+
+// PORT
+const port = process.env.PORT || 3000; // Assign custom assignable ports. 
+// To assign custom ports in terminal you can use keyword export(in windows = set). eg. export PORT=5001
+app.listen(port, () => console.log(`Listening the port ${port}...`))
