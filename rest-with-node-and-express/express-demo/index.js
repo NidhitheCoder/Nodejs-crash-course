a1cfde6ec7058f939abff08de11e0f3f4b6ae69e
Nidheesh PV <nidheesh@vonnue.com>
2024-01-01 20:55:17 +0530

    feat: implement get requert for courses and single course

--- rest-with-node-and-express/express-demo/index.js
+++ rest-with-node-and-express/express-demo/index.js
@@ -2,30 +2,53 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+// })
+
+// app.get('/api/courses', (req, res) => {
+//     res.send([1, 2, 3, 4])
+// })
+
+// // /api/courses/1 - single parameters
+// app.get('/api/courses/:id', (req, res) => {
+//     res.send(req.params.id)
+// })
+
+// // Request with multiple parameters
+// app.get('/api/posts/:year/:month', (req, res) => {
+//     res.send(req.params)
+// })
+
+// // Request with query parameters
+// app.get('/api/comments', (req, res) => {
+//     res.send(req.query)
+// })
+
+const courses = [
+    { id: 1, name: 'course 1' },
+    { id: 2, name: 'course 2' },
+    { id: 3, name: 'course 3' },
+    { id: 4, name: 'course 4' }
+]
+
+app.get('/', (_, res) => {
+    res.send('Welcome hello world')
 })
 
-app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3, 4])
+app.get('/api/courses', (_, res) => {
+    res.send(courses)
 })
 
-// /api/courses/1 - single parameters
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id)
-})
+    const course = courses.find(course => course.id === parseInt(req.params.id));
 
-// Request with multiple parameters
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.params)
-})
-
-// Request with query parameters
-app.get('/api/comments', (req, res) => {
-    res.send(req.query)
+    if (!course) res.send('The course with the given course ID was not found');
+    res.send(course);
 })
 
 // PORT
 const port = process.env.PORT || 3000; // Assign custom assignable ports. 
 // To assign custom ports in terminal you can use keyword export(in windows = set). eg. export PORT=5001
+
 app.listen(port, () => console.log(`Listening the port ${port}...`))
