00bb4c7de43ac58b0603ec080296fd3817e7e6d8
Nidheesh PV <nidheesh@vonnue.com>
2024-01-11 21:22:43 +0530

    feat: implemening vendor routes in adminroute

---
+++ food_order_backend/controllers/AdminController.ts
@@ -0,0 +1,19 @@
+import { Response, Request, NextFunction } from "express";
+
+export const createVender = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+export const getVendor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+export const getVenderByID = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
