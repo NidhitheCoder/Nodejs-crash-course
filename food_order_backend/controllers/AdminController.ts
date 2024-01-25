8eeff7a2b4a34ef817846dd9242fa38a72a7a9a6
Nidheesh PV <nidheesh@vonnue.com>
2024-01-25 21:38:10 +0530

    feat: add interface for create vendor

--- food_order_backend/controllers/AdminController.ts
+++ food_order_backend/controllers/AdminController.ts
@@ -1,10 +1,15 @@
 import { Response, Request, NextFunction } from "express";
+import { CreateVendorInput } from "../dto";
 
 export const createVender = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const { name, address, password, phone, email, foodType, ownerName } = <
+    CreateVendorInput
+  >req.body;
+};
 
 export const getVendor = async (
   req: Request,
--- food_order_backend/index.ts
+++ food_order_backend/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { AdminRoute, VendorRoute } from "./routes";
 
 const app = express();
@@ -6,6 +7,9 @@ const app = express();
 // app.use("/", (req, res) => {
 //   return res.send("Hello from food order backend");
 // });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/admin", AdminRoute);
 app.use("/vendor", VendorRoute);
