72b937cee88b838e6d50ccf31bca3b18f7fe9d20
NidhiTheCoder <nidhpv@gmail.com>
2024-03-28 22:00:11 +0530

    creating routes

--- store-api/app.js
+++ store-api/app.js
@@ -9,3 +9,22 @@ const errorMiddleware = require('./middleware/error-handler');
 
 // Middleware
 app.use(express.json())
+
+// Roots
+app.get('/', (req, res) => res.send('<h1>Store API</h1> <a href="api/v1/products" >Product Details</a>'))
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        // connect DB
+        app.listen(port, console.log(`Server is listening ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
