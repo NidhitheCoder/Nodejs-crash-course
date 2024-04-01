3bd893b5ce7d343899daed2f2dde780334ae54f6
NidhiTheCoder <nidhpv@gmail.com>
2024-04-01 23:17:31 +0530

    feat: add mongodb connect file

---
+++ store-api/db/connect.js
@@ -0,0 +1,3 @@
+const connectionString = 'mongodb+srv://Nidheesh:<password>@store-api-cluster.9qs1u3p.mongodb.net/?retryWrites=true&w=majority&appName=Store-api-Cluster'
+
+// mongodb+srv://Nidheesh:<password>@store-api-cluster.9qs1u3p.mongodb.net/
