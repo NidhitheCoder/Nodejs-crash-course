3a497f4af2f14dde71c2fcee1a49b2fdfbc70bfd
NidhiTheCoder <nidhpv@gmail.com>
2024-04-23 23:46:40 +0530

    Implement basic sort for products

--- store-api/controller/products.js
+++ store-api/controller/products.js
@@ -2,13 +2,15 @@ const Product = require('../models/product');
 
 const getAllProductStatic = async (req, res) => {
     // throw new Error('testing async errors package') // We can trow errors like this with async-errors package
-    const products = await Product.find({ featured: true });
+    // const products = await Product.find({ featured: true }); // default way
+
+    const products = await Product.find({}).sort('-name price'); // sort with -name and price
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
     console.log(req.query);
-    const { company, featured, search } = req.query;
+    const { company, featured, search, sort } = req.query;
     const queryObject = {};
 
     if (featured) queryObject.featured = featured === 'true';
@@ -16,7 +18,11 @@ const getAllProducts = async (req, res) => {
     if (search) queryObject.name = { $regex: search, $options: 'i' };
 
 
-    const products = await Product.find(queryObject);
+    let products = await Product.find(queryObject);
+
+    if (sort) {
+        products = products.sort(sort);
+    }
 
     res.status(200).json(products)
 }
