f6a266cce1e072935a229ef5e420e10305e17cc5
NidhiTheCoder <nidhpv@gmail.com>
2024-04-24 23:32:03 +0530

    Select needful fields only

--- store-api/controller/products.js
+++ store-api/controller/products.js
@@ -4,21 +4,23 @@ const getAllProductStatic = async (req, res) => {
     // throw new Error('testing async errors package') // We can trow errors like this with async-errors package
     // const products = await Product.find({ featured: true }); // default way
 
-    const products = await Product.find({}).sort('-name price'); // sort with -name and price
+    // const products = await Product.find({}).sort('-name price'); // sort with -name and price
+    const products = await Product.find({}).select('name price')
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
     console.log(req.query);
-    const { company, featured, search, sort } = req.query;
+    const { company, featured, search, sort, fields } = req.query;
     const queryObject = {};
 
     if (featured) queryObject.featured = featured === 'true';
     if (company) queryObject.company = { $regex: company, $options: 'i' };
     if (search) queryObject.name = { $regex: search, $options: 'i' };
 
-
-    let products = await Product.find(queryObject);
+    const selectFieldsString = fields.toString().replace(',', ' ')
+    console.log(selectFieldsString)
+    let products = await Product.find(queryObject).select(selectFieldsString);
 
     if (sort) {
         products = products.sort(sort);
