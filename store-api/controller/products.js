c78aa135de1ebff03194b80dc3c2292adb62370f
NidhiTheCoder <nidhpv@gmail.com>
2024-04-27 23:20:17 +0530

    implement page, limit, and skip

--- store-api/controller/products.js
+++ store-api/controller/products.js
@@ -5,7 +5,7 @@ const getAllProductStatic = async (req, res) => {
     // const products = await Product.find({ featured: true }); // default way
 
     // const products = await Product.find({}).sort('-name price'); // sort with -name and price
-    const products = await Product.find({}).select('name price')
+    const products = await Product.find({}).select('name price').limit(2).skip(2)
     res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -20,13 +20,25 @@ const getAllProducts = async (req, res) => {
 
 
     let result = Product.find(queryObject);
-
+    // Sort
     if (sort) {
         const sortList = sort.split(',').join(' ').trim();
         result = result.sort(sortList);
     } else {
         result = result.sort('createdAt');
     }
+
+    // Select fields
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ').trim();
+        result = result.select(fieldsList)
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req, query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit)
 
     const products = await result;
 
