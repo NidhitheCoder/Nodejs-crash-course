2e8dc77637f18f3b4a869e2e91520eccf17dad60
NidhiTheCoder <nidhpv@gmail.com>
2024-04-28 14:16:58 +0530

    Fix issue with pagination and limit

--- store-api/controller/products.js
+++ store-api/controller/products.js
@@ -10,7 +10,6 @@ const getAllProductStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    console.log(req.query);
     const { company, featured, search, sort, fields } = req.query;
     const queryObject = {};
 
@@ -35,7 +34,7 @@ const getAllProducts = async (req, res) => {
     }
 
     const page = Number(req.query.page) || 1;
-    const limit = Number(req, query.limit) || 10;
+    const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit;
 
     result = result.skip(skip).limit(limit)
