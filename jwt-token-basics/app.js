112c15a70d536cf39b25b60e1cf160d9f7dff8c1
NidhiTheCoder <nidhpv@gmail.com>
2024-05-06 21:29:47 +0530

    custom error middleware

--- jwt-token-basics/app.js
+++ jwt-token-basics/app.js
@@ -4,8 +4,15 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const notFoundMiddleware = require('./middlewares/not-found');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+
 
 // middleware
 console.log("Hello welcome home...");
+app.use(express.static('./public'));
+app.use(express.json());
 
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 app.use(express.json())
