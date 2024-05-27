5565a16837faffb089bb4a43e59832f905b05cc7
NidhiTheCoder <nidhpv@gmail.com>
2024-05-27 13:13:10 +0530

    Feat: basic authorisation for dashboard route

--- jwt-token-basics/controllers/main.js
+++ jwt-token-basics/controllers/main.js
@@ -3,7 +3,7 @@ const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
     const { username, password } = req.body;
-    console.log(username, password)
+
     // 3 ways to validate mongoose validation, Joi, check in the controller
     if (!username || !password) {
         throw new CustomAPIError('Please provide username and password', 400)
@@ -11,14 +11,27 @@ const login = async (req, res) => {
 
     const id = new Date().getDate();
 
-    const token = jwt.sign({ id, username })
-    res.send('Fake login router');
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    res.status(200).json({ msg: 'user created', token })
 }
 
 const dashboard = async (req, res) => {
-    const luckyNumber = Math.floor(Math.random() * 100);
+    const authHeader = req.headers.authorization;
 
-    res.status(200).json({ msg: `Hello ${req.query.name || 'User'}...`, secret: `Your secret lucky number is ${luckyNumber}` })
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomAPIError('No token Provided', 401)
+    }
+
+    const token = authHeader.split(' ')?.[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const luckyNumber = Math.floor(Math.random() * 100);
+
+        res.status(200).json({ msg: `Hello ${decoded.username}...`, secret: `Your secret lucky number is ${luckyNumber}` })
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to access this route', 401)
+    }
+
 }
 
 module.exports = {
