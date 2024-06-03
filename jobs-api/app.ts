650c7d6419f4bbf8afa1fe9965b19dd4d3d76022
NidhiTheCoder <nidhpv@gmail.com>
2024-06-03 10:16:26 +0530

    Feat: add rate limiter options

--- jobs-api/app.ts
+++ jobs-api/app.ts
@@ -15,12 +15,18 @@ import connectDB from "./connect/db";
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+  })
+);
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(rateLimiter);
 
 // Routes
 app.use("/api/v1/auth", authRoute);
