384cbc7319a58132d6db75b66259a69f86254578
nidhiTheCoder <nidhpv@gmail.com>
2025-03-21 23:40:30 +0530

    Fix: style and update UI

--- mupliplayer-game/rock-paper-scissors/client/src/client.js
+++ mupliplayer-game/rock-paper-scissors/client/src/client.js
@@ -1,10 +1,11 @@
-const sock = io();
 const writeMessage = (text) => {
     const contentDiv = document.querySelector('.content')
     const messagePara = document.createElement('p');
     messagePara.textContent = text;
-
+    
     contentDiv.appendChild(messagePara)
 }
 
+// initializing io object for socket
+const sock = io();
 sock.on('message', writeMessage)
